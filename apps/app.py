f45adefdce53d6ce5a829b4f022a346e1be78291
kunidesuyo <kunidesuyo1234@gmail.com>
2022-01-26 18:12:23 +0900

    loginout

--- apps/app.py
+++ apps/app.py
@@ -4,11 +4,20 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from apps.config import config
+from flask_login import LoginManager
 
 # SQLAlchemyをインスタンス化する
 db = SQLAlchemy()
 
 csrf = CSRFProtect()
+
+# LoginManagerをインスタンス化
+login_manager = LoginManager()
+# login_view属性に未ログイン時にリダイレクトするエンドポイントを指定する
+login_manager.login_view = "auth.signup"
+# login_message属性にログイン後に表示するメッセージを指定する
+# ここではなにも表示しないよう空を指定する
+login_manager.login_message = ""
 
 
 def create_app(config_key):
@@ -22,9 +31,18 @@ def create_app(config_key):
     db.init_app(app)
     # Migrateとアプリを連携する
     Migrate(app, db)
+    # login_managerをアプリケーションと連携する
+    login_manager.init_app(app)
 
     from apps.crud import views as crud_views
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
 
+    # これから作成するauthパッケージからviewsをimportする
+    from apps.auth import views as auth_views
+    # register_blueprintを使いviewsのauthをアプリへ登録する
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
+
     return app
+
+
